36475e29c90310e8905def882dfdc3e626a2eab5
Javen <107395637+Javen05@users.noreply.github.com>
2024-10-27 14:48:53 +0800

    Update question.js

--- question.js
+++ question.js
@@ -1,184 +1,175 @@
-htmlTitle = "Questionnaire"
-title = "Title for questionnaire"
+htmlTitle = "Questionnaire";
+title = "Feedback on Our Service";
 compulsoryQuestions = ["QN0", "RATING"];
 
 questions = [
   {
     number: "QN0",
     inputType: "display",
-    question: "Sample of a display question. If trigger is provided, click the Proceed button to activate trigger.",
-    hint: "Text below question that serves as sub-header. It is available but optional for all inputTypes.",
+    question: "Welcome to our feedback form! Please share your thoughts about our service.",
+    hint: "Your feedback is valuable to us!",
     trigger: "QN1"
   },
   {
     number: "QN1",
     inputType: "response",
-    question:
-      "number of first question must always be in compulsoryQuestions",
+    question: "What aspect of our service did you find most helpful?",
     options: [
-      { label: "Select me to trigger S1", value: 0, trigger: "S1" },
-      { label: "Option 1", value: 1 },
-      { label: "Option 2", value: 2 },
-      { label: "Option 3", value: 3 },
-      { label: "Option 4", value: 4 },
+      { label: "Customer Service", value: 0, trigger: "S1" },
+      { label: "Product Quality", value: 1 },
+      { label: "Timeliness", value: 2 },
+      { label: "Pricing", value: 3 },
+      { label: "Other", value: 4 },
     ],
     trigger: "QN2"
   },
   {
     number: "QN2",
     inputType: "sort",
-    question:
-      "Sorting/Ordering question. If individual trigger is specified, only the first option's trigger will be used. Else, use default trigger",
+    question: "Please rank the following attributes in order of importance:",
     options: [
-      { label: "Order 1", value: 1 },
-      { label: "Order 2", value: 2 },
-      { label: "Order 3 (Sort to top to trigger S2)", value: 3, trigger: "S2" },
-      { label: "Order 4", value: 4 },
+      { label: "Customer Support", value: 1 },
+      { label: "Value for Money", value: 2 },
+      { label: "Quality of Service", value: 3, trigger: "S2" },
+      { label: "Speed of Service", value: 4 },
     ],
     trigger: "QN3"
   },
   {
     number: "QN3",
     inputType: "checkbox",
-    question:
-      `Multiple selection question. Trigger for all selected options will be triggered. If no individual trigger is triggered, the default trigger will be used`,
+    question: "Which of the following features would you like to see in the future? (Select all that apply)",
     options: [
-      { label: "Option 1 (Trigger S3)", value: 1, trigger: "S3" },
-      { label: "Option 2 (Trigger S4)", value: 2, trigger: "S4" },
-      { label: "Option 3", value: 3 },
-      { label: "Option 4", value: 4 },
-      { label: "Option 5", value: 5 },
+      { label: "More Payment Options (Trigger S3)", value: 1, trigger: "S3" },
+      { label: "Expanded Product Range (Trigger S4)", value: 2, trigger: "S4" },
+      { label: "Faster Delivery Times", value: 3 },
+      { label: "Loyalty Program", value: 4 },
+      { label: "Customer Referral Discounts", value: 5 },
     ],
     trigger: "QN3a"
   },
   {
     number: "QN3a",
     inputType: "sort",
-    question:
-      "Rank these ratings in ordinal order",
+    question: "Rank the following services based on your experience:",
     options: [
-      { label: "Excellent", value: 1 },
-      { label: "Good", value: 2 },
-      { label: "Neutral", value: 3 },
-      { label: "Poor", value: 4 },
-      { label: "Very Poor", value: 5 },
+      { label: "Delivery Service", value: 1 },
+      { label: "Customer Support", value: 2 },
+      { label: "Product Range", value: 3 },
+      { label: "Online Experience", value: 4 },
+      { label: "Overall Satisfaction", value: 5 },
     ],
     trigger: "QN3b"
   },
   {
     number: "QN3b",
     inputType: "sort",
-    question:
-      "Rank these numbers in order",
+    question: "Rank the following aspects of our service from most to least satisfying:",
     options: [
-      { label: "3", value: 3 },
-      { label: "2", value: 2 },
-      { label: "4", value: 4 },
-      { label: "5", value: 5 },
-      { label: "1", value: 1 },
-      { label: "6", value: 6 }
+      { label: "Speed", value: 3 },
+      { label: "Quality", value: 2 },
+      { label: "Affordability", value: 1 },
+      { label: "Availability", value: 4 },
+      { label: "Support", value: 5 },
+      { label: "Variety", value: 6 }
     ],
     trigger: "QN3c"
   },
   {
     number: "QN3c",
     inputType: "sort",
-    question:
-      "Rank these numbers in order",
+    question: "Rank these options in terms of customer importance:",
     options: [
-      { label: "3", value: 3 },
-      { label: "2", value: 2 },
-      { label: "1", value: 1 },
-      
+      { label: "Flexibility", value: 3 },
+      { label: "Simplicity", value: 2 },
+      { label: "Comprehensiveness", value: 1 },
     ],
     trigger: "QN4"
   },
   {
     number: "QN4",
     inputType: "radio",
-    question: "Radio question. If default trigger is provided, user can produced without selecting.",
+    question: "Would you recommend our service to a friend?",
     options: [
-      { label: "Option 1", value: 1, trigger: "S5" },
-      { label: "Option 2", value: 2 },
-      { label: "Option 3", value: 3 },
-      { label: "Option 4", value: 4 },
+      { label: "Definitely", value: 1, trigger: "S5" },
+      { label: "Probably", value: 2 },
+      { label: "Not Sure", value: 3 },
+      { label: "Definitely Not", value: 4 },
     ],
     trigger: "QN5"
   },
   {
     number: "QN5",
     inputType: "dropdown",
-    question: "Dropdown question. Difference with radio is that dropdown REQUIRES selection for triggering default trigger.",
+    question: "How would you rate our service?",
     options: [
-      { label: "Option 1", value: 1, trigger: "S6" },
-      { label: "Option 2", value: 2 },
-      { label: "Option 3", value: 3 },
-      { label: "Option 4", value: 4 },
+      { label: "Excellent", value: 1, trigger: "S6" },
+      { label: "Good", value: 2 },
+      { label: "Average", value: 3 },
+      { label: "Poor", value: 4 },
     ],
     trigger: "QN6"
   },
   {
     number: "QN6",
     inputType: "radio",
-    question: "Question without trigger",
-    hint: "trigger is optional for ALL inputTypes",
+    question: "Would you visit us again?",
     options: [
-      { label: "Option 1", value: 1 },
-      { label: "Option 2", value: 2 },
-      { label: "Option 3", value: 3 },
-      { label: "Option 4", value: 4 },
+      { label: "Yes", value: 1 },
+      { label: "No", value: 2 },
+      { label: "Maybe", value: 3 },
     ],
   },
   {
     number: "S1",
     inputType: "display",
-    question: "This qn was triggered by QN 1.",
+    question: "Thank you for your feedback on Customer Service!",
   },
   {
     number: "S2",
     inputType: "display",
-    question: "This qn was triggered by sorting Order 3 to the top in QN2.",
+    question: "You valued Quality of Service in your ranking.",
   },
   {
     number: "S3",
     inputType: "display",
-    question: "You selected Option 1 for QN3",
+    question: "You are interested in more payment options.",
   },
   {
     number: "S4",
     inputType: "display",
-    question: "You selected Option 2 for QN3",
+    question: "You would like to see an expanded product range.",
   },
   {
     number: "S5",
     inputType: "display",
-    question: "You selected Option 1 for QN4",
+    question: "Thank you for recommending our service!",
   },
   {
     number: "S6",
     inputType: "display",
-    question: "You selected Option 1 for QN5",
+    question: "Thank you for your rating of Excellent!",
   },
   {
     number: "Conditional-1",
     inputType: "display",
-    question: "You selected Option 4 for ALL QNS",
+    question: "We are glad that you are satisfied with our service!",
   },
   {
     number: "Conditional-2",
-    inputType: "display",
-    question: "You selected at least one Option 3",
+    inputType: "response",
+    hint: "Share your experience and how you want us to improve",
+    question: "Please let us know how we can improve.",
   },
   {
     number: "Conditional-3",
     inputType: "display",
-    question: "Select all checkbox for QN3",
+    question: "You selected all options in the checkbox for QN3.",
   },
   {
     number: "RATING",
     inputType: "dropdown",
-    question:
-      "Rate this Questionnaire",
+    question: "Rate this Questionnaire",
     options: [
       { label: "Excellent", value: 1 },
       { label: "Good", value: 2 },
@@ -190,15 +181,15 @@ questions = [
 ];
 
 conditionalQuestions = {
-  /**
+  /** 
    * Question-triggered-if-conditions-met: [Option, Option1, Option2, Option3]
    * Option = [questionNumber, value that should be selected for the question]
    * sorting questions cannot be used in Option
    */
-  // by default, all conditions in array must be satisfied for a conditional question to trigger  
-  "Conditional-1": [["QN1", [4]], ["QN3", [4]], ["QN4", [4]], ["QN5", [4]], ["QN6", [4]]],
-  // by placing 'or' as last item of list, any condition satisfied will trigger the conditional question 
-  "Conditional-2": [["QN1", [3]], ["QN3", [3]], ["QN4", [3]], ["QN5", [3]], ["QN6", [3]], "or"],
+  // by default, all conditions in array must be satisfied for a conditional question to trigger
+  "Conditional-1": [["QN4", [1]], ["QN5", [1]], ["QN6", [1]]],
+  // by placing 'or' as last item of list, any condition satisfied will trigger the conditional question
+  "Conditional-2": [["QN4", [4]], ["QN5", [4]], ["QN6", [2]], "or"],
   // for checkbox qn, can have a list values to be selected to meet criteria.
-  "Conditional-3": [["QN3", [1,2,3,4,5]]]
+  "Conditional-3": [["QN3", [1, 2, 3, 4, 5]]]
 };
